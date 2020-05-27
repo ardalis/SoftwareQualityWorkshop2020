098be109058a0759fd00e3fa8324bd309ed0e097
Steve Smith <steve@kentsmiths.com>
2020-05-27 09:01:38 -0400

    Adding Gilded Rose

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# SoftwareQualityWorkshop2020
+# Software Quality Workshop 2020
+
 Public resources supporting private software quality workshops I'm delivering in 2020.
+
+## Learn More
+
+- [Pluralsight Courses](https://www.pluralsight.com/authors/steve-smith)
+- [Kata Catalog](https://github.com/ardalis/kata-catalog)
+- [devBetter Career Mentoring](https://devbetter.com)
+- [Weekly Dev Tips (YouTube)](https://www.youtube.com/weeklydevtips)
+
+## Original Kata Locations
+
+- [Gilded Rose](https://github.com/emilybache/GildedRose-Refactoring-Kata)
---
+++ gildedrose/ApprovalTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using System;
+using System.IO;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace csharpcore
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalTest
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            var output = fakeoutput.ToString();
+
+            Approvals.Verify(output);
+        }
+    }
+}
